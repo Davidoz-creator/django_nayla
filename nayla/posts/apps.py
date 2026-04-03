cf1d6b7d1629438b8fe86695e7ab53f3fe5eb133
Davidoz-creator <ozdgreatone@gmail.com>
2026-04-04 00:10:51 +0100

    commit_update

--- nayla/posts/apps.py
+++ nayla/posts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NaylaappConfig(AppConfig):
+class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'naylaapp'
+    name = 'posts'
